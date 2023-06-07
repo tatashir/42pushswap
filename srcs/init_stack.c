2e77353e3ec31d4a762c1f54a13183b526dea2fc
Taichi Tashiro <tatashir@c1r8s6.42tokyo.jp>
2023-06-07 15:38:55 +0900

    initstack

--- srcs/init_stack.c
+++ srcs/init_stack.c
@@ -3,53 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   init_stack.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: tatashir <tatashir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/08 15:58:10 by tkirihar          #+#    #+#             */
-/*   Updated: 2021/12/18 01:19:46 by tkirihar         ###   ########.fr       */
+/*   Created: 2023/05/13 18:32:23 by tatashir          #+#    #+#             */
+/*   Updated: 2023/06/07 15:36:45 by tatashir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-// 数値の重複があるか確認する関数
-static bool	check_dup(int num, int *num_list, size_t list_size)
+static bool	check_dup(int n, int *nlist, size_t listsize)
 {
 	size_t	i;
 
 	i = 1;
-	while (i < list_size)
+	while (i < listsize)
 	{
-		if (num == num_list[i])
+		if (n == nlist[i])
 			return (true);
 		i++;
 	}
 	return (false);
 }
 
-// スタックABを初期化する関数
-int	init_stack(t_stack *stack_a, t_stack *stack_b, int ac, char **av)
+int	init_stack(t_stack *stack_a, t_stack *stack_b, int argc, char **argv)
 {
 	size_t	i;
 	size_t	j;
 	int		tmp;
 
-	stack_a->num = (int *)malloc(sizeof(int) * ac);
+	stack_a->num = (int *)malloc(sizeof(int) * argc);
 	if (stack_a->num == NULL)
 		return (1);
-	stack_b->num = (int *)malloc(sizeof(int) * ac);
+	stack_b->num = (int *)malloc(sizeof(int) * argc);
 	if (stack_b->num == NULL)
 	{
 		free(stack_a->num);
 		return (1);
 	}
-	stack_a->top = ac - 1;
+	stack_a->top = argc - 1;
 	stack_b->top = 0;
 	i = 1;
-	j = ac - 1;
-	while (i <= (size_t)ac - 1)
+	j = argc - 1;
+	while (i <= (size_t)argc - 1)
 	{
-		tmp = ft_atoi(av[j--]);
+		tmp = ft_atoi(argv[j--]);
 		if (check_dup(tmp, stack_a->num, i))
 			return (1);
 		stack_a->num[i++] = tmp;
