e7639f6ca66b52c584b30b8bc9a0901182e81f2b
taichi tashiro <taichi@taichinoMacBook-Air.local>
2023-06-16 21:59:56 +0900

    make

--- srcs/main.c
+++ srcs/main.c
@@ -24,18 +24,18 @@ int	main(int argc, char **argv)
 	}
 
 	//newargc, newargv
-	int newargc;
-	char **newargv;
+	//int newargc;
+	//char **newargv;
 
-	newargc = 3;
+	//newargc = 3;
 	
 
-	if (init_stack(&a, &b, newargc, newargv))
+	if (init_stack(&a, &b, argc, argv))
 	{
 		ft_putendl_fd("Error", 2);
 		exit (1);
 	}
-	sort(newargc - 1, &a, &b);
+	sort(argc - 1, &a, &b);
 	free(a.num);
 	free(b.num);
 	return (0);
